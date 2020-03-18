0a6e205dab84514413fa9c640e03441ccb0cddb1
Michael Aldridge <michael.aldridge@betterhelp.com>
2020-03-18 15:10:41 -0700

    security-groups: Vault needs bidirectional 8201/tcp

--- modules/security-groups/sg_vault_server.tf
+++ modules/security-groups/sg_vault_server.tf
@@ -15,3 +15,23 @@ resource "aws_security_group_rule" "vault_api_client" {
   security_group_id        = aws_security_group.vault_server.id
   source_security_group_id = each.value
 }
+
+resource "aws_security_group_rule" "vault_server_sync" {
+  description       = "Accept server-to-server sync traffic"
+  type              = "ingress"
+  from_port         = 8201
+  to_port           = 8201
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vault_server.id
+  self              = true
+}
+
+resource "aws_security_group_rule" "vault_server_sync_egress" {
+  description       = "Permit server-to-server sync traffic"
+  type              = "egress"
+  from_port         = 8201
+  to_port           = 8201
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vault_server.id
+  self              = true
+}
